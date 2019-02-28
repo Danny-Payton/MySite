20ab8892ef392ed44e48855960d7f1934d9ef270
Danny-Payton <ytf123.payton.daniel@gmail.com>
2019-02-27 22:17:21 -0800

    Live Coding

--- TypingCode.js
+++ TypingCode.js
@@ -1,10 +1,40 @@
+var code = "var letter = 0;\n" +
+    "var code = \"Hello World\";\n" +
+    "var time = 500;\n" +
+    "var $window = $(window);\n" +
+    "var $type = $(\"#typeCode\")\n" +
+    "var counter = 0;\n" +
+    "\n" +
+    "$window.scroll(inView);\n" +
+    "\n" +
+    "function type() {\n" +
+    "    if (letter < code.length) {\n" +
+    "        document.getElementById(\"typeCode\").innerHTML += code.charAt(letter);\n" +
+    "        letter++;\n" +
+    "        setTimeout(type, time);\n" +
+    "    }\n" +
+    "}\n" +
+    "\n" +
+    "function inView() {\n" +
+    "    var windowHeight = $window.height();\n" +
+    "    var windowBottom = $window.scrollTop() + windowHeight;\n" +
+    "    var typeTop = $type.offset().top;\n" +
+    "\n" +
+    "    console.log(typeTop);\n" +
+    "    console.log(windowBottom);\n" +
+    "\n" +
+    "    if (typeTop <= windowBottom && counter == 0) {\n" +
+    "        type();\n" +
+    "        counter=1;\n" +
+    "    }\n" +
+    "}";
 var letter = 0;
-var code = 'Lorem ipsum typing effect!';
-var time = 50;
+var time = 40;
 var $window = $(window);
 var $type = $("#typeCode")
+var counter = 0;
 
-$window.on('scroll', inView());
+$window.scroll(inView);
 
 function type() {
     if (letter < code.length) {
@@ -22,7 +52,8 @@ function inView() {
     console.log(typeTop);
     console.log(windowBottom);
 
-    if (typeTop <= windowBottom) {
+    if (typeTop <= windowBottom && counter == 0) {
         type();
+        counter = 1;
     }
 }
